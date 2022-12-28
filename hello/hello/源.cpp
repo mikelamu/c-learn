75eee4fc26327b3fc406795aa010470839e6eafe
mikelamu <1994716623@qq.com>
2022-12-28 22:46:28 +0800

    -all

---
+++ hello/hello/源.cpp
@@ -0,0 +1,13 @@
+/*
+git bash notes
+ git config user.name 跟你的用户名
+ git config user.email 你的邮箱
+ git add 提交到缓存仓库
+ git status 查看状态
+ git rm –cached 可以返回暂存区的文件
+ git commit –m “备注信息” filename 提交到本地仓库
+ 远程仓库配置
+ git remote add origin https://github.com/mikelamu/c-learn.git
+ git push –u origin master
+ 克隆命令： git clone [远程仓库链接]
+*/
