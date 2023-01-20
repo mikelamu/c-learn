74d88c4e463988fe1692a2d8a8f7a5483ee3a037
mikelamu <1994716623@qq.com>
2023-01-20 22:57:02 +0800

    2023_01_20

--- hello/hello/2_1 string容器.cpp
+++ hello/hello/2_1 string容器.cpp
@@ -7,8 +7,42 @@
 			string(const string&str) 使用一个string对象初始化另一个string对象
 			string(int n, char c) 使用n个字符c初始化
 		赋值：
-			直接 = 赋值
-			利用assign赋值
+			- string& operator=(const char* s);             //char*类型字符串 赋值给当前的字符串
+			- string& operator=(const string &s);         //把字符串s赋给当前的字符串
+			- string& operator=(char c);                          //字符赋值给当前的字符串
+			- string& assign(const char *s);                  //把字符串s赋给当前的字符串
+			- string& assign(const char *s, int n);     //把字符串s的前n个字符赋给当前的字符串
+			- string& assign(const string &s);              //把字符串s赋给当前字符串
+			- string& assign(int n, char c);                  //用n个字符c赋给当前字符串
+		拼接：
+			- string& operator+=(const char* str);                   //重载+=操作符
+			- string& operator+=(const char c);                         //重载+=操作符
+			- string& operator+=(const string& str);                //重载+=操作符
+			- string& append(const char *s);                               //把字符串s连接到当前字符串结尾
+			- string& append(const char *s, int n);                 //把字符串s的前n个字符连接到当前字符串结尾
+			- string& append(const string &s);                           //同operator+=(const string& str)
+			- string& append(const string &s, int pos, int n);//字符串s中从pos开始的n个字符连接到字符串结尾
+		查找：
+			int find(const string& str,int pos=0); 查找str第一次出现位置，从pos开始查找，没找到返回-1
+			int rfind(const string& str,int pos=0); 从右往左查找str最后一次出现位置，从pos开始查找
+		替换：
+			int replace(int pos, int n, const string& str); 替换从pos开始n个字符 为字符串str
+			int replace(int pos, int n, const char* s); 替换从pos开始n个字符 为字符串s
+		比较：
+			按照字符的ASCII进行比较
+			int compare(const string &s) const; 与字符串s进行比较，相等为零
+			int compare(const char *s) const;
+		字符存取：
+			通过[] at 访问单个字符
+			char& operator[](int n); 
+			char& at(int n);
+		插入和删除：
+			string& insert(int pos, const char* s);
+			string& insert(int pos, const string& str);
+			string& insert(int pos, int n, const char c);
+			string& erase(int pos, int n = npos); 删除从pos开始的n个字符
+		获取子串：
+			string substr(int pos = 0, int n = npos) const 返回pos开始的n个字符组成的字符串
 */
 #include<iostream>
 using namespace std;
--- hello/hello/2_2 vector.cpp
+++ hello/hello/2_2 vector.cpp
@@ -1,11 +1,30 @@
 /*
-	vector
+	vector 结构和数组非常类似，称为单端数组
+		不同之处数组是静态空间，vector可以动态扩展：开辟一个新空间，并将原有数据拷贝进去，释放旧空间
 	算法：for_each
 	迭代器： vector<int>::iterator
 	语法 vector<type>v;
 		首元素指针 v.begin() 
 		尾元素下一地址指针 v.end()
 		尾插 v.push_back()
+	构造函数：
+		vector<T> v; //采用模板实现类实现，默认构造函数
+		vector(v.begin(), v.end()); //将v[begin(), end())区间中的元素拷贝给本身。
+		vector(n, elem); //构造函数将n个elem拷贝给本身。
+		vector(const vector &vec); //拷贝构造函数。
+	赋值：
+		vector& operator=(const vector &vec); //重载等号操作符
+		assign(beg, end); //将[beg, end)区间中的数据拷贝赋值给本身。
+		assign(n, elem); //将n个elem拷贝赋值给本身。
+	容量与大小：
+		empty(); //判断容器是否为空
+		capacity(); //容器的容量
+		size(); //返回容器中元素的个数
+		resize(int num); //重新指定容器的长度为num，若容器变长，则以默认值0填充新位置。
+		//如果容器变短，则末尾超出容器长度的元素被删除。
+		resize(int num, elem); //重新指定容器的长度为num，若容器变长，则以elem值填充新位置。
+		//如果容器变短，则末尾超出容器长度的元素被删除
+	插入：
 */
 #include<iostream>
 using namespace std;
